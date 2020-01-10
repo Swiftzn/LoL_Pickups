9dffa96ace7172a31a3436af7d174fa31f1236ad
David Allen <David.Allen@gcc.co.uk>
2020-01-10 17:30:14 +0000

    Add project files.

---
+++ LoL_Pickups/MyCommands.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using DSharpPlus;
+using DSharpPlus.CommandsNext;
+using DSharpPlus.CommandsNext.Attributes;
+using MingweiSamuel.Camille;
+using MingweiSamuel.Camille.Enums;
+
+namespace LoL_Pickups
+{
+    public class MyCommands
+    {
+        [Command("lg")]
+        [Aliases("lastgame")]
+        public async Task lastgame(CommandContext ctx, string summonername)
+        {
+            await ctx.TriggerTypingAsync();
+            var riotApi = RiotApi.NewInstance("RGAPI-c0bf4888-0b86-42e9-a5c0-335bc4388701");
+            var summonerData = await riotApi.SummonerV4.GetBySummonerNameAsync(Region.EUW, summonername);
+            var matchlist = await riotApi.MatchV4.GetMatchlistAsync(Region.EUW, summonerData.AccountId, endIndex: 1);
+            var matchDataTasks = matchlist.Matches.Select(matchMetadata => riotApi.MatchV4.GetMatchAsync(Region.EUW, matchMetadata.GameId)).ToArray();
+            var matchDatas = await Task.WhenAll(matchDataTasks);
+
+            for (var i = 0; i < matchDatas.Count(); i++)
+            {
+                var matchData = matchDatas[i];
+                var participantIdData = matchData.ParticipantIdentities.First(pi => summonerData.Id.Equals(pi.Player.SummonerId));
+                var participant = matchData.Participants.First(p => p.ParticipantId == participantIdData.ParticipantId);
+
+                var win = participant.Stats.Win;
+                var champ = (Champion)participant.ChampionId;
+                var k = participant.Stats.Kills;
+                var d = participant.Stats.Deaths;
+                var a = participant.Stats.Assists;
+                var cs = (participant.Stats.NeutralMinionsKilled + participant.Stats.TotalMinionsKilled);
+                var kda = (k + a) / (float)d;
+                await ctx.RespondAsync($"Your last game was with {champ.Name()} and your KDA was {k}/{d}/{a} ({kda:0.00}) with {cs} CS");
+            }
+        }
+
+    }
+}
---
+++ LoL_Pickups/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Threading.Tasks;
+using DSharpPlus;
+using DSharpPlus.CommandsNext;
+
+namespace LoL_Pickups
+{
+    class Program
+    {
+        static DiscordClient discord;
+        static CommandsNextModule commands;
+
+        static void Main(string[] args)
+        {
+            MainAsync(args).ConfigureAwait(false).GetAwaiter().GetResult();
+        }
+
+        static async Task MainAsync(string[] args)
+        {
+            discord = new DiscordClient(new DiscordConfiguration
+            {
+                Token = "NjY0MTE1OTM4MTE4ODYwODAx.XhSYWA.WBj4yyQTcZYn70XFwD6iFlyorEM",
+                TokenType = TokenType.Bot,
+                UseInternalLogHandler = true,
+                LogLevel = LogLevel.Debug
+            });
+
+            discord.MessageCreated += async e =>
+            {
+                if (e.Message.Content.ToLower().StartsWith("ping"))
+                    await e.Message.RespondAsync("pong!");
+            };
+
+            commands = discord.UseCommandsNext(new CommandsNextConfiguration
+            {
+                StringPrefix = "!"
+            });
+
+            commands.RegisterCommands<MyCommands>();
+
+            await discord.ConnectAsync();
+            await Task.Delay(-1);
+        }
+    }
+}
