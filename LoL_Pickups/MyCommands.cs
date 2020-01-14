d85538702380586ee9a4ec33f663f68c4933f181
David Allen <dallenzn@gmail.com>
2020-01-14 17:18:18 +0000

    Fixed issue where bot was answerings all messages not just DM's

--- LoL_Pickups/MyCommands.cs
+++ LoL_Pickups/MyCommands.cs
@@ -1,9 +1,6 @@
 ﻿using System;
-using System.Collections.Generic;
 using System.Linq;
-using System.Text;
 using System.Threading.Tasks;
-using DSharpPlus;
 using DSharpPlus.CommandsNext;
 using DSharpPlus.CommandsNext.Attributes;
 using MingweiSamuel.Camille;
@@ -19,19 +16,18 @@ namespace LoL_Pickups
         [Aliases("lastgame")]
         public async Task lastgame(CommandContext ctx, string summonername)
         {
+            await ctx.Message.DeleteAsync();
             await ctx.TriggerTypingAsync();
-            var riotApi = RiotApi.NewInstance("Riot API Token");
+            var riotApi = RiotApi.NewInstance("RGAPI-bd0fcb72-1257-41e5-86e2-0fadb18c4800");
             var summonerData = await riotApi.SummonerV4.GetBySummonerNameAsync(Region.EUW, summonername);
             var matchlist = await riotApi.MatchV4.GetMatchlistAsync(Region.EUW, summonerData.AccountId, endIndex: 1);
             var matchDataTasks = matchlist.Matches.Select(matchMetadata => riotApi.MatchV4.GetMatchAsync(Region.EUW, matchMetadata.GameId)).ToArray();
             var matchDatas = await Task.WhenAll(matchDataTasks);
-
             for (var i = 0; i < matchDatas.Count(); i++)
             {
                 var matchData = matchDatas[i];
                 var participantIdData = matchData.ParticipantIdentities.First(pi => summonerData.Id.Equals(pi.Player.SummonerId));
                 var participant = matchData.Participants.First(p => p.ParticipantId == participantIdData.ParticipantId);
-
                 var win = participant.Stats.Win;
                 var champ = (Champion)participant.ChampionId;
                 var k = participant.Stats.Kills;
@@ -39,7 +35,7 @@ namespace LoL_Pickups
                 var a = participant.Stats.Assists;
                 var cs = (participant.Stats.NeutralMinionsKilled + participant.Stats.TotalMinionsKilled);
                 var kda = (k + a) / (float)d;
-                await ctx.RespondAsync($"Your last game was with {champ.Name()} and your KDA was {k}/{d}/{a} ({kda:0.00}) with {cs} CS");
+                await ctx.RespondAsync($"{ctx.Member.DisplayName}, your last game was with {champ.Name()} and your KDA was {k}/{d}/{a} ({kda:0.00}) with {cs} CS");
             }
         }
 
@@ -48,17 +44,18 @@ namespace LoL_Pickups
         {
             await ctx.Message.DeleteAsync();
             var interactivity = ctx.Client.GetInteractivityModule();
-            await ctx.Member.SendMessageAsync("What is your Summoner Name");
-            var msg = await interactivity.WaitForMessageAsync(xm => xm.Author.Id == ctx.Member.Id, TimeSpan.FromMinutes(1));
-            var riotApi = RiotApi.NewInstance("Riot API Token");
+            var dmchannel = await ctx.Client.CreateDmAsync(ctx.Member);
+            await dmchannel.SendMessageAsync("What is your Summoner Name");
+            var msg = await interactivity.WaitForMessageAsync(xm => xm.Author.Id == ctx.Member.Id & xm.Channel == dmchannel, TimeSpan.FromMinutes(1));
+            var riotApi = RiotApi.NewInstance("RGAPI-bd0fcb72-1257-41e5-86e2-0fadb18c4800");
             var summonerData = await riotApi.SummonerV4.GetBySummonerNameAsync(Region.EUW, msg.Message.Content);
             var embed = new DiscordEmbedBuilder
             {
                 Title = "Summoner Info",
                 Color = DiscordColor.Red
             };
-            embed.AddField("Summoner Name", $"{summonerData.Name}");
-            embed.AddField("Summoner Level", $"{summonerData.SummonerLevel}");
+            embed.AddField("Summoner Name", $"{summonerData.Name}" );
+            embed.AddField("Summoner Level", $"{summonerData.SummonerLevel}", true);
             await ctx.Member.SendMessageAsync(embed: embed);
         }
     }
--- LoL_Pickups/Program.cs
+++ LoL_Pickups/Program.cs
@@ -1,51 +1,133 @@
 ﻿using System;
+using System.IO;
+using System.Text;
 using System.Threading.Tasks;
 using DSharpPlus;
 using DSharpPlus.CommandsNext;
+using DSharpPlus.CommandsNext.Exceptions;
+using DSharpPlus.Entities;
+using DSharpPlus.EventArgs;
 using DSharpPlus.Interactivity;
+using Newtonsoft.Json;
 
 namespace LoL_Pickups
 {
     public class Program
     {
-        static DiscordClient discord;
-        static CommandsNextModule commands;
-        static InteractivityModule interactivity;
+        public DiscordClient Client { get; set; }
+        public CommandsNextModule Commands { get; set; }
+        static InteractivityModule Interactivity { get; set; }
 
         static void Main(string[] args)
         {
-            MainAsync(args).ConfigureAwait(false).GetAwaiter().GetResult();
+            var prog = new Program();
+            prog.RunBotAsync().GetAwaiter().GetResult();
         }
 
-        static async Task MainAsync(string[] args)
+        public async Task RunBotAsync()
         {
-            discord = new DiscordClient(new DiscordConfiguration
-            {
-                Token = "Discord Token",
-                TokenType = TokenType.Bot,
-                UseInternalLogHandler = true,
-                LogLevel = LogLevel.Debug
-            });
+            using var fs = File.OpenRead("config.json");
+            using var sr = new StreamReader(fs, new UTF8Encoding(false));
+            string json = await sr.ReadToEndAsync();
 
-            discord.MessageCreated += async e =>
+            var cfgjson = JsonConvert.DeserializeObject<ConfigJson>(json);
+            var cfg = new DiscordConfiguration
             {
-                if (e.Message.Content.ToLower().StartsWith("ping"))
-                    await e.Message.RespondAsync("pong!");
+                Token = cfgjson.Token,
+                TokenType = TokenType.Bot,
+                AutoReconnect = true,
+                LogLevel = LogLevel.Debug,
+                UseInternalLogHandler = true
             };
 
-            commands = discord.UseCommandsNext(new CommandsNextConfiguration
+            this.Client = new DiscordClient(cfg);
+            this.Client.Ready += this.Client_Ready;
+            this.Client.GuildAvailable += this.Client_GuildAvailable;
+            this.Client.ClientErrored += this.Client_ClientErrored;
+
+            this.Client.UseInteractivity(new InteractivityConfiguration
             {
-                StringPrefix = "!"
+                PaginationBehaviour = TimeoutBehaviour.Ignore,
+                PaginationTimeout = TimeSpan.FromMinutes(5),
+                Timeout = TimeSpan.FromMinutes(2)
             });
 
-            interactivity = discord.UseInteractivity(new InteractivityConfiguration
+            var ccfg = new CommandsNextConfiguration
             {
+                StringPrefix = cfgjson.CommandPrefix,
+                EnableDms = true,
+                EnableMentionPrefix = true
+            };
 
-            });
+            this.Commands = this.Client.UseCommandsNext(ccfg);
+            this.Commands.CommandExecuted += this.Commands_CommandExecuted;
+            this.Commands.CommandErrored += this.Commands_CommandErrored;
 
-            commands.RegisterCommands<MyCommands>();
-            await discord.ConnectAsync();
+            this.Commands.RegisterCommands<MyCommands>();
+
+            await this.Client.ConnectAsync();
             await Task.Delay(-1);
         }
+
+        private Task Client_Ready(ReadyEventArgs e)
+        {
+            e.Client.DebugLogger.LogMessage(LogLevel.Info, "LoLPickups", "Client is ready to process events.", DateTime.Now);
+            return Task.CompletedTask;
+        }
+
+        private Task Client_GuildAvailable(GuildCreateEventArgs e)
+        {
+            e.Client.DebugLogger.LogMessage(LogLevel.Info, "LoLPickups", $"Guild available: {e.Guild.Name}", DateTime.Now);
+            return Task.CompletedTask;
+        }
+
+        private Task Client_ClientErrored(DSharpPlus.EventArgs.ClientErrorEventArgs e)
+        {
+            e.Client.DebugLogger.LogMessage(LogLevel.Error, "LoLPickups", $"Exception occured: {e.Exception.GetType()}: {e.Exception.Message}", DateTime.Now);
+            return Task.CompletedTask;
+        }
+
+        private Task Commands_CommandExecuted(CommandExecutionEventArgs e)
+        {
+            e.Context.Client.DebugLogger.LogMessage(LogLevel.Info, "LoLPickups", $"{e.Context.User.Username} successfully executed '{e.Command.QualifiedName}'", DateTime.Now);
+            return Task.CompletedTask;
+        }
+
+        private async Task Commands_CommandErrored(CommandErrorEventArgs e)
+        {
+            // let's log the error details
+            e.Context.Client.DebugLogger.LogMessage(LogLevel.Error, "ExampleBot", $"{e.Context.User.Username} tried executing '{e.Command?.QualifiedName ?? "<unknown command>"}' but it errored: {e.Exception.GetType()}: {e.Exception.Message ?? "<no message>"}", DateTime.Now);
+
+            // let's check if the error is a result of lack
+            // of required permissions
+            if (e.Exception is ChecksFailedException ex)
+            {
+                // yes, the user lacks required permissions, 
+                // let them know
+
+                var emoji = DiscordEmoji.FromName(e.Context.Client, ":no_entry:");
+
+                // let's wrap the response into an embed
+                var embed = new DiscordEmbedBuilder
+                {
+                    Title = "Access denied",
+                    Description = $"{emoji} You do not have the permissions required to execute this command.",
+                    Color = new DiscordColor(0xFF0000) // red
+                };
+                await e.Context.RespondAsync("", embed: embed);
+            }
+        }
+
+    }
+    public struct ConfigJson
+    {
+        [JsonProperty("token")]
+        public string Token { get; private set; }
+
+        [JsonProperty("prefix")]
+        public string CommandPrefix { get; private set; }
+
+        [JsonProperty("lolapi")]
+        public string Lolapi { get; private set; }
     }
 }
